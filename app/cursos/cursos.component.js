cf46707472f90863e8ee6545536b89ffe60e3b59
ordnaelmedeiros <lrmarkanjo@gmail.com>
2016-07-16 15:22:29 -0300

    Alteração Curso

--- app/cursos/cursos.component.js
+++ app/cursos/cursos.component.js
@@ -12,12 +12,58 @@ var core_1 = require('@angular/core');
 var cursos_service_1 = require('./cursos.service');
 var CursosComponent = (function () {
     function CursosComponent(cursosService) {
+        this.msgErro = "";
+        this.idCount = 0;
+        this.cursos = new Array();
+        this.curso = new Curso();
         this.nome = "loiane.training";
-        this.teste = { id: 1, nome: 'Ivana' };
-        this.cursos = cursosService.getCursos();
+        //this.cursos = cursosService.getCursos();
+        //this.txtNome.nativeElement.focus();
     }
-    CursosComponent.prototype.mostrar = function (teste) { alert(teste.nome); };
-    ;
+    CursosComponent.prototype.salvar = function () {
+        this.msgErro = "";
+        if (this.curso.nome == null || this.curso.nome == '') {
+            this.msgErro = "Campo Obrigatório";
+        }
+        else {
+            if (this.curso.id == null) {
+                this.idCount++;
+                this.curso.id = this.idCount;
+                this.cursos.push(this.curso);
+                this.curso = new Curso();
+            }
+            else {
+                for (var index = 0; index < this.cursos.length; index++) {
+                    var element = this.cursos[index];
+                    if (element.id == this.curso.id) {
+                        element.id = this.curso.id;
+                        element.nome = this.curso.nome;
+                        break;
+                    }
+                }
+            }
+        }
+        this.txtNome.nativeElement.focus();
+    };
+    CursosComponent.prototype.excluir = function (cur) {
+        for (var index = 0; index < this.cursos.length; index++) {
+            var element = this.cursos[index];
+            if (element.id == cur.id) {
+                this.cursos.splice(index, 1);
+                break;
+            }
+        }
+        this.txtNome.nativeElement.focus();
+    };
+    CursosComponent.prototype.editar = function (cur) {
+        this.curso.id = cur.id;
+        this.curso.nome = cur.nome;
+        this.txtNome.nativeElement.focus();
+    };
+    __decorate([
+        core_1.ViewChild('txtNome'), 
+        __metadata('design:type', Object)
+    ], CursosComponent.prototype, "txtNome", void 0);
     CursosComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -34,10 +80,5 @@ var Curso = (function () {
     function Curso() {
     }
     return Curso;
-}());
-var Teste = (function () {
-    function Teste() {
-    }
-    return Teste;
 }());
 //# sourceMappingURL=cursos.component.js.map
--- app/cursos/cursos.component.ts
+++ app/cursos/cursos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { CursosService } from './cursos.service';
 
@@ -10,27 +10,79 @@ import { CursosService } from './cursos.service';
 })
 export class CursosComponent {
     
-    cursos : Array<Curso>;
+    @ViewChild('txtNome') txtNome;
 
-    nome : string = "loiane.training";
+    msgErro:String = "";
 
-    teste : Teste = {id: 1, nome:'Ivana'};
+    idCount:number = 0;
 
-    mostrar(teste:Teste) { alert(teste.nome); };
+    cursos:Array<Curso> = new Array();
 
+    curso:Curso = new Curso();
+
+    nome:string = "loiane.training";
 
     constructor(cursosService: CursosService) {
-        this.cursos = cursosService.getCursos();
+        //this.cursos = cursosService.getCursos();
+        //this.txtNome.nativeElement.focus();
+    }
+
+    salvar() {
+
+        this.msgErro = "";
+
+        if (this.curso.nome == null || this.curso.nome == '' ) {
+
+            this.msgErro = "Campo Obrigatório"
+
+        } else {
+
+            if (this.curso.id==null) {
+
+                this.idCount ++;
+                this.curso.id = this.idCount;
+                this.cursos.push(this.curso);
+                this.curso = new Curso();
+
+            } else {
+
+                for (var index = 0; index < this.cursos.length; index++) {
+                    var element:Curso = this.cursos[index];
+                    if (element.id == this.curso.id) {
+                        element.id = this.curso.id;
+                        element.nome = this.curso.nome;
+                        break;
+                    }
+                }
+
+            }
+            
+        }
+
+        this.txtNome.nativeElement.focus();
+
+    }
+
+    excluir(cur:Curso) {
+        for (var index = 0; index < this.cursos.length; index++) {
+            var element:Curso = this.cursos[index];
+            if (element.id == cur.id) {
+                this.cursos.splice(index, 1);
+                break;
+            }
+        }
+        this.txtNome.nativeElement.focus();
+    }
+
+    editar(cur:Curso) {
+        this.curso.id = cur.id;
+        this.curso.nome = cur.nome;
+        this.txtNome.nativeElement.focus();
     }
 
 }
 
 class Curso {
-    id : number;
-    nome : string;
-}
-
-class Teste {
     id : number;
     nome : string;
 }
